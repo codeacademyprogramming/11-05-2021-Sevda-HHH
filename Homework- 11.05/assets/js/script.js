732d2970c673262c2026bff5fe67e1132d00018f
Sevda-HHH <SevdaMh@code.edu.az>
2021-05-11 23:26:20 +0400

    Added  Show Password checkbox

--- Homework- 11.05/assets/js/script.js
+++ Homework- 11.05/assets/js/script.js
@@ -27,6 +27,19 @@ $(document).ready(function () {
                         window.location = "file:///C:/Users/OMEN/Desktop/Homework-%20May%2011/11-05-2021-Sevda-HHH/Homework-%2011.05/index.html"
                     }
                 })
+                var passs = $("#usrPass").val()
+                return passs
+
             }
-        )
+        ).then(a => {
+            $("#showPass").click(function () {
+                if ($(this).attr("class") == "form-check-input checked") {
+                    $("#usrPass").attr("type", "password")
+                    $(this).removeClass("checked")
+                } else {
+                    $(this).addClass("checked")
+                    $("#usrPass").attr("type", "text")
+                }
+            })
+        })
 })
